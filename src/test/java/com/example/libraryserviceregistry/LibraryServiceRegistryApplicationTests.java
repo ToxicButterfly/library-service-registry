18aaa7f77f4e444e9a8bc5bc457395c327013956
Verlygo Dmitry <56788178+ToxicButterfly@users.noreply.github.com>
2023-12-04 20:38:34 +0300

    Initial commit

---
+++ src/test/java/com/example/libraryserviceregistry/LibraryServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
